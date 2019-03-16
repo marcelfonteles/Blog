55ef557abff88b2211c52b36ef9010c72c702a42
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-16 13:13:43 +0000

    comments and has_many association

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @post = 'batata'
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
