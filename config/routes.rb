ae70378bc345f8505842ca8642a8b6498c02c79d
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-16 16:06:06 +0000

    adding comment with post customize routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments, except: [:create, :new]
   get '/comment/:id/new', to: 'comments#new', as: 'new_comment'
-  post '/comment/:id/create', to: 'comments#create', as: 'create_comment'
+  post '/comment/:post_id/create', to: 'comments#create', as: 'create_comment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
