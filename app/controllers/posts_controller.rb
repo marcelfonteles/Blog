23ce9774cf8da53f057de4403a34a83a945ea5fb
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-16 13:59:45 +0000

    controller action for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -17,4 +24,14 @@ class PostsController < ApplicationController
 
   def destroy
   end
+  private
+    def posts_params
+      params.require(:post).permit(:title, :content)
+    end
+    
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+
 end
